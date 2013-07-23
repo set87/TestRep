ba03f92180fc223dac2646db423c692add322018
set87 <bpavlyk87@gmail.com>
2013-07-23 21:02:05 +0300

    2

--- src/test/java/com/silentest/webdriver/FirstTest.java
+++ src/test/java/com/silentest/webdriver/FirstTest.java
@@ -1,19 +1,20 @@
 package com.silentest.webdriver;
 
 import org.junit.Test;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class FirstTest {
 
 
-    @Test
-    public void startWebDriver(){
-      WebDriver driver = new FirefoxDriver();
+	@Test
+	public void startWebDriver() {
+		WebDriver driver = new FirefoxDriver();
 
-      driver.navigate().to("http://www.poppin.com/");
-        org.junit.Assert.assertTrue("http://www.poppin.com/",
+		driver.navigate().to("http://www.poppin.com/");
+		Assert.assertTrue("http://www.poppin.com/",
                 driver.getTitle().startsWith("dasdasdasd"));
-        driver.close();
-    }
+		driver.close();
+	}
 }
